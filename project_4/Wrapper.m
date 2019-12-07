16d1f4c414afe5a5f1a4aa8ca994ffb651ed5b5e
Shreya Suresh <shreyasuresh12@gmail.com>
2019-12-07 14:34:23 -0500

    Add files via upload

--- project_4/Wrapper.m
+++ project_4/Wrapper.m
@@ -6,46 +6,51 @@ clc
 clear all
 close all
 import gtsam.*
-
 %% Add ToolBox to Path eg. ToolboxPath = 'gtsam_toolbox';
-ToolboxPath = 'gtsam_toolbox';
+ToolboxPath = 'D:\UMCP\Courses\CMSC426\P4_CV\gtsam_toolbox';
 addpath(ToolboxPath);
-
 
 %% Load Data
 % Download data from the following link: 
 % https://drive.google.com/open?id=1ZFXZEv4yWgaVDE1JD6-oYL2KQDypnEUU
 load('DataSquareNew.mat');
+%load('DataMappingNew.mat');
 load('CalibParams.mat');
 
 %% SLAM Using GTSAM
 %[LandMarksComputed, AllPosesComputed] = SLAMusingGTSAM(DetAll, K, TagSize, qIMUToC, TIMUToC,...
-%                                                IMU, LeftImgs, TLeftImgs);
-
-
-%XRYR1 = inv(K)*inv(H)*[X1(1) Y1(1)]
-
-%% estimating pose
-
+                                                %IMU, LeftImgs, TLeftImgs);
+                                                
+%%Estimating pose for origin (R)
 F1=DetAll{1,1};
 X1 = [F1(30,2), F1(30,4), F1(30,6), F1(30,8)];
 Y1 = [F1(30,3), F1(30,5), F1(30,7),F1(30,9)];
 %plot(X1,Y1)
 
-XR = [0,0+TagSize,0+TagSize,0];
-YR = [0,0,0-TagSize,0-TagSize];
-%plot(XR,YR)
+XR = [0,TagSize,TagSize,0];
+YR = [0,0,-TagSize,-TagSize];
 
 H = est_homography(XR,YR,X1,Y1);
-temp = inv(K)*H;
-h1 = temp(:,1);
-h2 = temp(:,2);
-h3 = temp(:,3);
 
-KH = [h1 h2 h1.*h2];
-R0 = svd(KH)
-T0 = h3/(norm(h1))
-x0 = [R0 T0]
+temp = inv(K)*H;
+%temp = H;
+% h1 = temp(:,1);
+% h2 = temp(:,2);
+% h3 = temp(:,3);
+% KH = [h1 h2 cross(h1,h2)];
+h1 = temp(1,:);
+h2 = temp(2,:);
+h3 = temp(3,:);
+
+KH = [h1' h2' cross(h1',h2')];
+
+[U, Sigma, V] = svd(KH);
+Sigma = [1 0 0; 0 1 0; 0 0 det(U*V')];
+R0 = U*Sigma*V';
+%R0 = KH;
+T0 = h3'/(norm(h1'));
+%T0 = h3/(norm(h1));
+x0 = [R0 T0];
 %loc = -R0' * T0
 %pose = [loc,R0]
 
@@ -97,13 +102,19 @@ for i= 2:length(DetAll)
 
     H = est_homography(XR,YR,X1,Y1);
     homo = inv(K)*H;
-    h1 = homo(:,1);
-    h2 = homo(:,2);
-    h3 = homo(:,3);
-
-    KH = [h1 h2 h1.*h2];
-    R = svd(KH);
-    T = h3/(norm(h1));
+%     h1 = homo(:,1);
+%     h2 = homo(:,2);
+%     h3 = homo(:,3);
+    h1 = homo(1,:);
+    h2 = homo(2,:);
+    h3 = homo(3,:);
+    KH = [h1' h2' cross(h1',h2')];
+    %KH = [h1 h2 cross(h1,h2)];
+    [U, Sigma, V] = svd(KH);
+    Sigma = [1 0 0; 0 1 0; 0 0 det(U*V')];
+    R = U*Sigma*V';
+    %T = h3/(norm(h1));
+    T = h3'/(norm(h1'));
     x = [R T];
     camPose{1,i} = x;
     
@@ -114,32 +125,16 @@ for i= 2:length(DetAll)
         [x2,y2] = apply_homography(H, curr(4), curr(5));
         [x3,y3]= apply_homography(H, curr(6), curr(7));
         [x4,y4] = apply_homography(H, curr(8), curr(9));
-        temp1 = [IC(j,1) x1 y1 x2 y2 x3 y3 x4 y4 ];
+        temp1 = [IC(j,1) x1 y1 x2 y2 x3 y3 x4 y4];
         temp = [temp; temp1];
         
     end
     DetAllReal{1,i}= temp;
 end
-
-
-
-
-
-
-
-
-%% Create graph container and add factors to it
-% graph = NonlinearFactorGraph;
-% 
-% %% Add prior to fix first pose as origin
-% priorMean = Pose2(0,0,0); % prior at origin
-% priorNoise = noiseModel.Diagonal.Sigmas([0.3; 0.3; 0.1]);
-% graph.add(PriorFactorPose2(0, priorMean, priorNoise));
-% %graph.add(PriorFactorPose2(1, priorMean, priorNoise));
-% 
-% %% Add odometry factors
-% model = noiseModel.Diagonal.Sigmas([0.2; 0.2; 0.1]);
-% graph.add(BetweenFactorPose2(0, 1, Pose2(2, 0, 0 ), model));
-% graph.add(BetweenFactorPose2(1, 2, Pose2(2, 0, pi/2), model));
-% graph.add(BetweenFactorPose2(2, 3, Pose2(2, 0, pi/2), model));
-% graph.add(BetweenFactorPose2(3, 4, Pose2(2, 0, pi/2), model));
+    for i = 1:length(camPose)
+        %hold on;
+        %plot3(camPose{1, i}(:,1:3),camPose{1, i}(:,4), [1;1;1])
+        quat = rotm2quat(camPose{1, i}(:,1:3));
+        plotTransforms(camPose{1, i}(:,4)', quat);
+        hold on;
+    end
